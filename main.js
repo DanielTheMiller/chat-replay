e35df9d3585c5439ce50de55e79b1d122b12a9cb
Daniel Miller <daniel9miller6@gmail.com>
2025-01-20 00:07:16 +0000

    Dynamic Padding Adjustment

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ chatInputBox = document.getElementById("input_line");
 chat_list = document.getElementById("chat_list");
 chat_zone = document.getElementById("chat_zone");
 
+latest_chat_record = null;
 
 const magic_8_ball_lines = ["It is certain", "Reply hazy, try again", "Don’t count on it", "It is decidedly so", "Ask again later",
 "My reply is no", "Without a doubt", "Better not tell you now", "My sources say no", "Yes definitely", "Cannot predict now", "Outlook not so good",
@@ -16,31 +17,52 @@ chatInputBox.addEventListener('keypress', function (e) {
         console.log(insertedText)
         // Clear input
         chatInputBox.value = "";
-        // Add to the chat history array
-        chat_history.push(insertedText);
         // Insert a new chat bubble
-        addChat("guest", insertedText, "00:00");
+        addChat("guest", insertedText, "NOW");
         random_magic_8_ball_reply();
     }
 });
 
 function addChat(actor, text, time)
 {
+    if (time == "NOW") 
+    {
+        time = new Date()
+        time = `${time.getHours()}:${time.getMinutes()}`;
+    }
     let new_list_item = document.createElement("li");
     new_list_item.innerText = text;
     new_list_item.className = "chat " + actor;
     chat_list.appendChild(new_list_item);
+    if (latest_chat_record != null && 
+        latest_chat_record["time"] == time) 
+    { // Cosy the new chat and the previous chat together
+        if (latest_chat_record["actor"] == actor) {
+            latest_chat_record.element.className += " cosy";
+        }
+    }
+    else
+    {
+        let time_span = document.createElement("span");
+        time_span.className = "time";
+        time_span.innerText = time;
+        new_list_item.prepend(time_span);
+    }
+    // Add to the chat history array
+    latest_chat_record = {"actor": actor, "time": time, "text": text, "element": new_list_item}
+    chat_history.push(latest_chat_record);
     // Scroll the chat window down
     chat_zone.scrollTop = chat_zone.scrollHeight;
 }
 
 function introduction()
 {
-    addChat("host", "Welcome to Chat Replay!", "00:00");
-    setTimeout(() => addChat("host", "I am wise Lily. I know all", "00:00"), 1000);
-    setTimeout(() => addChat("host", "Fortunes... futures...", "00:00"), 3000);
-    setTimeout(() => addChat("host", "And everything in-between!", "00:00"), 5000);
-    setTimeout(() => addChat("host", "Ask me any yes or no question", "00:00"), 7000);
+    chatInputBox.focus();
+    addChat("host", "Welcome to Chat Replay!", "NOW");
+    setTimeout(() => addChat("host", "I am wise Lily. I know all", "NOW"), 1000);
+    setTimeout(() => addChat("host", "Fortunes... futures...", "NOW"), 3000);
+    setTimeout(() => addChat("host", "And everything in-between!", "NOW"), 5000);
+    setTimeout(() => addChat("host", "Ask me any yes or no question", "NOW"), 7000);
     // host_lines.forEach((v, index) =>  { setTimeout(() => addChat("host", v, "00:00"), 4000 * index+1 )});
 }
 
@@ -48,7 +70,7 @@ function random_magic_8_ball_reply()
 {
     let response_index= Math.floor(Math.random() * magic_8_ball_lines.length);
     let random_response = magic_8_ball_lines[response_index];
-    setTimeout(() => addChat("host", random_response, "00:00"), 1000 )   
+    setTimeout(() => addChat("host", random_response, "NOW"), 1000 )   
 }
 
 introduction();
